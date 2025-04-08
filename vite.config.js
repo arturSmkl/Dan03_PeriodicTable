d6895a3da13f6ed6ff85bc46922741d32e544987
Artur Smýkal <1133@student.itgymnazium.cz>
2025-04-08 16:38:25 +0200

    c3

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/build/',
+  base: '/Dan_03PeriodicTable/',
   plugins: [
     vue(),
     vueDevTools(),
